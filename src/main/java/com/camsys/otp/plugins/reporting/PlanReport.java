4fc12ed06f1f6a23dd0c41e1a29a62d3d0ca1e84
Simon Jacobs <sidaja@gmail.com>
2019-02-12 13:00:17 -0500

    first pass at plan report plugin

---
+++ src/main/java/com/camsys/otp/plugins/reporting/PlanReport.java
@@ -0,0 +1,90 @@
+package com.camsys.otp.plugins.reporting;
+
+
+import java.util.Date;
+
+// {"type_id":1, "from_lat":40.754910,"from_lng":-73.994102,"to_lat":40.754006,"to_lng":-73.988301,"request_time":"2018-11-01T00:00:00","server_time":"2018-11-01T00:00:00","arrive_by":1}
+public class PlanReport {
+
+    private static final int PLAN_TYPE = 1;
+
+    private int typeId = PLAN_TYPE;
+
+    private double fromLat;
+
+    private double fromLon;
+
+    private double toLat;
+
+    private double toLon;
+
+    private Date requestTime;
+
+    private Date serverTime;
+
+    private boolean arriveBy;
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public double getFromLat() {
+        return fromLat;
+    }
+
+    public void setFromLat(double fromLat) {
+        this.fromLat = fromLat;
+    }
+
+    public double getFromLon() {
+        return fromLon;
+    }
+
+    public void setFromLon(double fromLon) {
+        this.fromLon = fromLon;
+    }
+
+    public double getToLat() {
+        return toLat;
+    }
+
+    public void setToLat(double toLat) {
+        this.toLat = toLat;
+    }
+
+    public double getToLon() {
+        return toLon;
+    }
+
+    public void setToLon(double toLon) {
+        this.toLon = toLon;
+    }
+
+    public Date getRequestTime() {
+        return requestTime;
+    }
+
+    public void setRequestTime(Date requestTime) {
+        this.requestTime = requestTime;
+    }
+
+    public Date getServerTime() {
+        return serverTime;
+    }
+
+    public void setServerTime(Date serverTime) {
+        this.serverTime = serverTime;
+    }
+
+    public boolean isArriveBy() {
+        return arriveBy;
+    }
+
+    public void setArriveBy(boolean arriveBy) {
+        this.arriveBy = arriveBy;
+    }
+}
--- src/main/java/com/camsys/otp/plugins/reporting/PlanReportingService.java
+++ src/main/java/com/camsys/otp/plugins/reporting/PlanReportingService.java
@@ -1,9 +1,14 @@
 package com.camsys.otp.plugins.reporting;
 
 import com.camsys.otp.plugins.threads.AbstractThreadPoolPlugin;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.opentripplanner.api.resource.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Date;
 
 public class PlanReportingService extends AbstractThreadPoolPlugin<Response> {
 
@@ -11,8 +16,35 @@ public class PlanReportingService extends AbstractThreadPoolPlugin<Response> {
 
     @Override
     public void process(Response response) {
-        int nItineraries = response.getPlan() == null ? 0 : response.getPlan().itinerary.size();
-        _log.info("Received trip plan with {} itineraries.", nItineraries);
+        PlanReport report = new PlanReport();
+        String fromPlace = response.requestParameters.get("fromPlace");
+        String toPlace = response.requestParameters.get("toPlace");
+        String[] fromPlaceTokens = fromPlace.split(",");
+        String[] toPlaceTokens = toPlace.split(",");
+        if (fromPlaceTokens.length != 2 || toPlaceTokens.length != 2) {
+            _log.error("Invalid input: fromPlace={}, toPlace={}", fromPlace, toPlace);
+        }
+        Double fromLat = Double.parseDouble(fromPlaceTokens[0]);
+        Double fromLon = Double.parseDouble(fromPlaceTokens[1]);
+        Double toLat = Double.parseDouble(toPlaceTokens[0]);
+        Double toLon = Double.parseDouble(toPlaceTokens[1]);
+        report.setFromLat(fromLat);
+        report.setFromLon(fromLon);
+        report.setToLat(toLat);
+        report.setToLon(toLon);
+        Boolean arriveBy = Boolean.parseBoolean(response.requestParameters.get("arriveBy"));
+        report.setArriveBy(arriveBy);
+        report.setRequestTime(response.debugOutput.getRequestDate());
+        report.setServerTime(new Date(response.debugOutput.getStartedCalculating()));
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(
+                PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+        try {
+            String reportJson = mapper.writeValueAsString(report);
+            _log.info("Plan report: {}", reportJson);
+        } catch(JsonProcessingException ex) {
+            _log.error("Error processing json: {}", ex);
+        }
     }
 
     @Override
